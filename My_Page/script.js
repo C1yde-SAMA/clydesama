b9c4005bb5e78fd705cb82c2ebf88ccbcea07d7c
C1yde-SAMA <147669763+C1yde-SAMA@users.noreply.github.com>
2024-09-08 21:08:58 +1000

    v1.1

--- My_Page/script.js
+++ My_Page/script.js
@@ -1,28 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     changeLanguage('en'); // 默认设置为英语
-
-    const loadingScreen = document.getElementById('loading-screen');
-    const mainContent = document.getElementById('main-content');
-
-    // // 检查元素是否存在
-    // if (loadingScreen && mainContent) {
-
-    //     // 监听动画结束事件
-    //     // loadingScreen.addEventListener('animationend', () => {
-    //     //     // 将 loading-screen 的透明度逐渐变为 0，实现淡出效果
-    //     //     loadingScreen.style.transition = 'opacity 0.5s ease-in-out';
-    //     //     loadingScreen.style.opacity = '0';  // 淡出加载屏幕
-
-    //     //     // 在淡出动画结束后，隐藏加载屏幕并显示主内容
-    //     //     setTimeout(() => {
-    //     //         loadingScreen.classList.add('hidden');  // 隐藏加载屏幕
-    //     //         mainContent.style.opacity = '1'; // 显示主页面内容
-    //     //     }, 500);  // 使用与过渡时间相同的 0.5s
-    //     // });
-
-    // } else {
-    //     console.error('Loading screen or main content element not found.');
-    // }
 });
 
 function changeLanguage(language) {
